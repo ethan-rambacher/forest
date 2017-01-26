e78fd736e6f3ad3f1a0b20d906a0ecc72f41e9f1
Ethan Rambacher <eerambach@gmail.com>
2017-01-26 16:42:11 -0500

    Added server.js

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express')
-const app = express()
+const path = require('path')  
+const exphbs = require('express-handlebars')
+
 const port = 3000
+const app = express()
+
+app.engine('.hbs', exphbs({  
+	  defaultLayout: 'main',
+	  extname: '.hbs',
+	  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+
+app.set('view engine', '.hbs')
+
+app.set('views', path.join(__dirname, 'views'))  
+
+app.use((request, response, next) => {
+	console.log(request.headers)
+	next()
+})
+
 
 app.get('/', (request, response) => {
-	response.send('Hello world!')
+	console.log(request.url)
+	response.send("hello world")
 })
 
 
